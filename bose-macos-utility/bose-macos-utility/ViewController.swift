e20979636f8ffc66b13e97d4b38ad4741577a21f
lukasz-zet <21034618+lukasz-zet@users.noreply.github.com>
2021-06-23 23:14:23 +0200

    add the initial base xcode project for App

---
+++ bose-macos-utility/bose-macos-utility/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bose-macos-utility
+//
+//  Created by Łukasz Zalewski on 23/06/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
