c059200eef8b408dbe50e42df0c01dd638368281
lukasz-zet <21034618+lukasz-zet@users.noreply.github.com>
2021-06-23 23:37:37 +0200

    implement a basic version of menu with quit button; set the icon; add one todo

--- bose-macos-utility/bose-macos-utility/AppDelegate.swift
+++ bose-macos-utility/bose-macos-utility/AppDelegate.swift
@@ -9,12 +9,30 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    // Keep a reference to the status bar item to keep it alive throughout the whole lifetime of the application
+    var statusBarItem: NSStatusItem!
+    var statusBarMenu: NSMenu!
     
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // Initalize the menu bar extra
+        let statusBar = NSStatusBar.system
+        
+        let statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        self.statusBarItem = statusBarItem
+        // TODO: use the template image, so that the icon is changed when the dark/light mode settings change
+        statusBarItem.button?.title = "🎧"
+        let statusBarMenu = NSMenu(title: "first menu")
+        self.statusBarMenu = statusBarMenu
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.addItem(withTitle: "Quit",
+                              action: #selector(quit),
+                              keyEquivalent: "")
+    }
+    
+    @objc func quit() {
+        print("Quitting the menu...")
+        self.statusBarMenu.cancelTracking()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
